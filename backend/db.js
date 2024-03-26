b0f5701b093ebf1cab9e4ab4d026c661781ee791
Ash V <96707067+tvermaashutosh@users.noreply.github.com>
2024-03-26 12:25:55 +0530

    Minor changes db.js

--- backend/db.js
+++ backend/db.js
@@ -9,7 +9,7 @@ const mongoConnect = async () => {
       "@cluster0.amuso8q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     )
     .then(() => {
-      console.log("---     Successfully connected to mongo!     ---");
+      console.log("Successfully connected to mongo!");
     })
     .catch((err) => {
       console.log("Error while connecting to mongo...", err);

737a6989781ce5b0ae6c36e2c898166d2eaf64fa
Arcturus22 <amitsharma89460@gmail.com>
2024-04-15 13:43:11 +0530

    Updated Extras Route

--- backend/routes/extra.js
+++ backend/routes/extra.js
@@ -5,21 +5,20 @@ const Student = require("../models/Student");
 
 // extras detail
 router.post(
-  "/:studentId",
+  "/create",
   passport.authenticate("jwt", { session: false }),
   async (req, res) => {
     try {
-      const { studentId } = req.params;
-      const { dish, price, quantity } = req.body;
+      const { rollNo, dish, price, quantity } = req.body;
 
-      const student = await Student.findById(studentId);
+      const student = await Student.findOne({ rollNo: rollNo });
 
       if (!student) {
         return res.status(404).json({ error: "Student not found" });
       }
 
       const extra = await Extras.create({
-        student: studentId,
+        student: student._id,
         dish,
         price,
         quantity,
@@ -31,6 +30,38 @@ router.post(
       return res.status(200).json(extra);
     } catch (error) {
       console.error("Error updating extras", error);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+);
+
+// Get all extras and extrasBill of a student
+router.get(
+  "/:studentId/get/all",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      const { studentId } = req.params;
+
+      const student = await Student.findById(studentId).populate("extrasTaken");
+
+      if (!student) {
+        return res.status(404).json({ error: "Student not found" });
+      }
+
+      const extrasTaken = student.extrasTaken;
+      if (extrasTaken.length === 0) {
+        return res.status(400).json({ error: "No extras taken" });
+      }
+      extrasBill = 0;
+
+      extrasTaken.forEach((extra) => {
+        extrasBill += extra.price * extra.quantity;
+      });
+
+      return res.status(200).json({extrasTaken, extrasBill});
+    } catch (error) {
+      console.error("Error fetching all extras of student", error);
       return res.status(500).json({ error: "Internal Server Error" });
     }
   }

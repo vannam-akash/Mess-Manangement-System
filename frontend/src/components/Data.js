3cd0ffa22ac58dc512825e07ce1af70ce59aac31
Abhishekgupta204 <guptaabhishek9875@gmail.com>
2024-05-02 16:41:12 +0530

    Modified css for student bills data

--- frontend/src/components/Data.js
+++ frontend/src/components/Data.js
@@ -4,20 +4,19 @@ import styles from "./Data.module.css";
 const Data = () => {
   return (
     <>
-    <div class={styles.container}>
-    <div class={styles.row1}>
-      <div class={styles.column1}>10</div>
-      <div class={styles.column1}>500</div>
-      <div class={styles.column1}>3000</div>
-    </div>
-    </div>
-    <div class={styles.container}>
-    <div class={styles.row2}>
-      <div class={styles.column2}>Number of meals canceled</div>
-      <div class={styles.column2}>Total extras amount</div>
-      <div class={styles.column2}>Total mess bill</div>
-    </div>
-    </div>
+      <div class={styles.container}>
+        <div class={styles.row1}>
+          <div class={styles.column1}>10</div>
+          <div class={styles.column1}>500</div>
+          <div class={styles.column1}>3000</div>
+        </div>
+
+        <div class={styles.row2}>
+          <div class={styles.column2}>Number of meals canceled</div>
+          <div class={styles.column2}>Total extras amount</div>
+          <div class={styles.column2}>Total mess bill</div>
+        </div>
+      </div>
     </>
   );
 };
